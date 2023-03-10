676f61e15da1e9f6d4d201ff99fe9ca539a1350f
Mahmudur Rahman <hridoymahmud71@gmail.com>
2023-03-10 15:20:44 +0600

    auth started

---
+++ src/auth/user.js
@@ -0,0 +1,10 @@
+const { sendResponse } = require("../functions/index");
+
+const handler = async (event) => {
+  console.log(event.requestContext);
+  return sendResponse(200, event.requestContext.authorizer.claims.email);
+};
+
+module.exports = {
+  handler,
+};
